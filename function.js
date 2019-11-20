823d4eddb5e31435e07ad9cca19622bd66a57454
ahmadalarif <44748381+ahmadalarif@users.noreply.github.com>
2019-11-20 09:52:18 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,68 +1,74 @@
-let youRock1 = "samar"
-let youRock2 = "sara"
-function youRock(name) {
-  
-    console.log("You Rock " + name);
-  }
-  ​
-  var name = "ahmed";
-  youRock(name);
-  
+
+// 11.  Define a `youRock` function that accepts a string argument of a name and returns a string using that name.
+// ```js
+// youRock('Samar') // 'You rock Samar!'
+// youRock('Sara') //'You rock Sara!'
+// ```
+
+// 2.  Define a `square` function that accepts a number argument and returns that number multipled by itself.
+// ```js
+// square(2) // 4
+// square(3) // 9
+// ```
+
+// 3.  Define a `cube` function that accepts a number argument and returns that number raised to the third power.
+// ```js
+// cube(2) // 8
+// cube(3) // 27
+// ```
+
+// 4.  Define a `toTheFourth` function that accepts a number argument and returns that number raised to the fourth power.
+// ```js
+// toTheFourth(2) // 16
+// toTheFourth(3) // 81
+// ```
+
+// #### Extra Practice
+
+// If you finish the Lab, try this challenge.
+
+// Write a function that will add, subtract, multiply or divide two numbers and return the answer 
+// ```js
+// calculator(1, 2, "add") // should return 3 
+// calculator(1, 2, "subtract") // should return -1
+// calculator(1, 2, "divide") // should return .5
+// calculator(1, 2, "multiply") // should return 2
+// calculator(1, 2, "something else") // should return "calculator can only add, subtract, divide, or multiply
+// calculator("cat", 2, "add") // should return "calculator only accepts numbers"
+// ```
 
 
+// ### Lab: FizzBuzz Function
 
-function square(num) {
-  
-    num1 =  num * num ;
-    console.log(num1); 
-    }
-    ​
-    var num = 2;
-    square(num);
-    
-    function cube(num) {
-  
-        num1 =  num * num * num ;
-        console.log(num1); 
-        }
-        ​
-        var num = 3;
-        cube(num);
-        
-        function toTheFourth(num) {
-  
-            num1 =  num * num * num * num;
-            console.log(num1); 
-            }
-            ​
-            var num = 3;
-            toTheFourth(num);
-            ​
-            
-            function calc(num1,num2,math) {
-                if (math == "add"){
-                 var num = num1 + num2 ;
-                  console.log(num);
-                }else if (math == "sub"){
-                     var num = num1 - num2 ;
-                  console.log(num);
-                }else if (math == "div"){
-                     var num = num1 / num2 ;
-                  console.log(num);
-                }else if (math == "mul"){
-                     var num = num1 * num2 ;
-                  console.log(num);
-                }else if (math !== "add" || math !== "sub" || math !== "div" || math !== "mul"){
-                     
-                  console.log("calculator can only add, subtract, divide, or multiply");
-                }else if ( sNaN(num2) !== true) {
-                  console.log("calculator only accepts numbers");
-                }
-              ​
-              }
-              ​
-              var num1 ;
-              var num2 ;
-              var math ;
-              calc(3 , "e" , "sub");
-              
+// Write a function that accepts an argument of a number
+
+// If it is a multiple of 3, return “Fizz” instead of the number.
+
+// If it is a multiple of 5, return “Buzz” instead of the number.
+
+// If it is a multiple of both 3 and 5, return “FizzBuzz” instead of the number.
+
+// Otherwise, return the number
+
+// ```js
+// fizzBuzz(3) // Fizz
+// fizzBuzz(15) // FizzBuzz
+// fizzBuzz(7) // 7
+// ```
+
+// ### Lab: RainDrop Function
+
+// Write a function that accepts an argument of a number
+
+// If the number contains 3 as a factor, output 'Pling'.
+
+// If the number contains 5 as a factor, output 'Plang'.
+
+// If the number contains 7 as a factor, output 'Plong'.
+
+// If the number does not contain 3, 5, or 7 as a factor, output the number as a string.
+
+// ```js
+// rainDrop(28) // Plong
+// rainDrop(1755) // PlingPlang
+// rainDrop(34) // 34
